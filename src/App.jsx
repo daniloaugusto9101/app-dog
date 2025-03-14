4abdb392c6f08cbddc703f0a99717e2322554784
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-03-14 20:25:07 -0300

    feat: Add hook useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Login from "./components/Login/Login";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 
-const App = () => {
+function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
-};
+}
 
 export default App;
